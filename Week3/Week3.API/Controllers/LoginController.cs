c88a0277b1f29e301d0f2338261cf7cbe464ed0a
Turab Kemiksiz <turabkemiksiz@gmail.com>
2021-12-25 11:19:39 +0300

    First Commit
    Geçen hafta ödevinin üzerine memory cache yerine distrubed cache eklendi

---
+++ Week3/Week3.API/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using System;
+using Week3.Model;
+using Week3.Model.User;
+using Week3.Service.User;
+
+namespace Week3.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        private readonly IUserService userService;
+        private readonly IDistributedCache distributedCache;
+
+        public LoginController( IUserService _userService, IDistributedCache _distributedCache)
+        {
+            userService = _userService;
+            distributedCache = _distributedCache;
+        }
+
+        [HttpPost]
+        public General<bool> Login([FromBody] UserLoginViewModel loginUser)
+        {
+            var cachedData = distributedCache.GetString("LoginUser");
+
+            General<bool> response = new() { Entity = false };
+            General<UserLoginViewModel> result = userService.Login(loginUser);
+
+            if (result.IsSuccess)
+            {
+                var cacheOptions = new DistributedCacheEntryOptions()
+                {
+                    AbsoluteExpiration = DateTime.Now.AddMinutes(1)
+                };
+
+                if (string.IsNullOrEmpty(cachedData))
+                {
+                    distributedCache.SetString("LoginUser", JsonConvert.SerializeObject(result.Entity), cacheOptions);
+                }
+
+                response.Entity = true;
+                response.IsSuccess = true;
+                response.Message = "İşlem başarılı";
+            }
+            else
+            {
+                response.ExceptionMessage = "Tekrar giriş yapın";
+            }
+
+            return response;
+        }
+    }
+}
---
+++ Week3/Week3.API/Infrastructure/LoginFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using Week3.Model.User;
+
+namespace Week3.API.Infrastructure
+{
+    public class LoginFilter : Attribute, IActionFilter
+    {
+        private readonly IDistributedCache distributedCache;
+        public LoginFilter(IDistributedCache _distributedCache)
+        {
+            distributedCache = _distributedCache;
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            return;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)//, IServiceProvider serviceProvider)
+        {
+
+            var cachedData = distributedCache.GetString("LoginUser");
+
+            //var memoryCache = context.HttpContext.RequestServices.GetService<IMemoryCache>();
+
+            if(string.IsNullOrEmpty(cachedData))
+            {
+                context.Result = new UnauthorizedObjectResult("Lütfen önce giriş yapın!");
+            }
+
+            
+        }
+    }
+}
